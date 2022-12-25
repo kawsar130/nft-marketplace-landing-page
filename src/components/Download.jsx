acebea2801048c164a673fd88c394088daeeddf4
Kawsar Ahmed <58148356+kawsar130@users.noreply.github.com>
2022-12-26 01:45:18 +0600

    Landing Page Complete

---
+++ src/components/Download.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import styles from '../styles/Global';
+import assets from '../assets';
+
+const Download = () => {
+  return (
+    <div className={`${styles.section} ${styles.bgWhite}`}>
+      <div className={`${styles.subSection} flex-col text-center`}>
+        <div>
+          <h1 className={`${styles.h1Text} ${styles.blackText}`}>
+            Download the Source Code
+          </h1>
+          <p className={`${styles.pText} ${styles.blackText}`}>
+            Get the full source code on GitHub
+          </p>
+        </div>
+        <button className={styles.btnPrimary}>
+          <a
+            target="_blank"
+            href="https://github.com/kawsar130/NFT-Market-Place-with-React-Native"
+            rel="noreferrer"
+          >
+            Source Code
+          </a>
+        </button>
+        <div className={styles.flexCenter}>
+          <img
+            src={assets.scene}
+            alt="download_png"
+            className={styles.fullImg}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Download;
